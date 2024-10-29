95462bad422a0729e5a5e1464e221f931fb1b2ff
Antucan <pl2023269@365.stucom.com>
2024-10-29 10:06:28 +0100

    updating

--- exercici01_antoniohp.js
+++ exercici01_antoniohp.js
@@ -1,28 +1,29 @@
-fetch("exercici01_antoniohp.html");
-fetch("encerta.html");
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("play").onclick = function () {
+        let dir1 = document.getElementById("result");
+        let num = Math.floor(Math.random() * 11);
+        var numRand = document.getElementById('result').innerHTML = num;
+        if (numRand < 5) {
+            dir1.style.color = "red";
+            window.open("encerta.html");
+        } else if (numRand => 5) {
+            dir1.style.color = "green";
+            window.open("encerta.html");
+        }
+    };
 
-document.getElementById("play").onclick = function () {
-    let dir1 = document.getElementById("result");
-    let num = Math.floor(Math.random() * 11);
-    dir1.innerHTML = num;
-    if (num < 5) {
-        dir1.style.color = "red";
-        window.open("encerta.html");
-    } else if (num => 5) {
-        dir1.style.color = "green";
-        window.open("encerta.html");
+    document.getElementById("compare").onclick = function () {
+        let numUser = parseInt(document.getElementById("num").value);
+        let numOriginal = document.getElementById("result").innerHTML;
+        let div1 = document.getElementById("guess");
+        let div2 = document.getElementById("resolve");
+        if (result === numRand) {
+            div1.innerHTML = "The numbers are equals";
+            div2.innerHTML = "The numbers are equals";
+        } else {
+            div1.innerHTML = "The numbers doesn't match";
+            div2.innerHTML = "The numbers doesn't match";
+        }
     }
-}
-document.getElementById("compare").onclick = function () {
-    let numOriginal = document.getElementById("result");
-    let numUser = document.getElementById("num");
-    let div1 = document.getElementById("guess");
-    let div2 = document.getElementById("resolve");
-    if (numOriginal === numUser) {
-        div1.innerHTML = "The numbers are equals";
-        div2.innerHTML = "The numbers are equals";
-    } else {
-        div1.innerHTML = "The numbers doesn't match";
-        div2.innerHTML = "The numbers doesn't match";
-    }
-}
+})
+
