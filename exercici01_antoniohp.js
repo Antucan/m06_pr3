a538b4a81f1a3ed359909e89ee47fde7ffb5d7eb
Antucan <pl2023269@365.stucom.com>
2024-10-29 12:21:06 +0100

    updating

--- exercici01_antoniohp.js
+++ exercici01_antoniohp.js
@@ -1,29 +1,44 @@
-document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("play").onclick = function () {
-        let dir1 = document.getElementById("result");
-        let num = Math.floor(Math.random() * 11);
-        var numRand = document.getElementById('result').innerHTML = num;
-        if (numRand < 5) {
-            dir1.style.color = "red";
-            window.open("encerta.html");
-        } else if (numRand => 5) {
-            dir1.style.color = "green";
-            window.open("encerta.html");
-        }
-    };
+let numRand;
+let div1;
+let div2;
 
-    document.getElementById("compare").onclick = function () {
-        let numUser = parseInt(document.getElementById("num").value);
-        let numOriginal = document.getElementById("result").innerHTML;
-        let div1 = document.getElementById("guess");
-        let div2 = document.getElementById("resolve");
-        if (result === numRand) {
-            div1.innerHTML = "The numbers are equals";
-            div2.innerHTML = "The numbers are equals";
-        } else {
-            div1.innerHTML = "The numbers doesn't match";
-            div2.innerHTML = "The numbers doesn't match";
-        }
+function generate() {
+    div1 = document.getElementById("result");
+    numRand = Math.floor(Math.random() * 10);
+    div1.innerHTML = numRand;
+    if (numRand < 5) {
+        div1.style.color = "red";
+    } else {
+        div1.style.color = "green";
     }
-})
+    const newWindow = window.open("encerta.html", "", "width=500,height=500");
+    newWindow.opener.document.getElementById("result");
+
+};
+function compare() {
+    let numUser = parseInt(document.getElementById("num").value);
+    let numParent = window.parent.getElementById("result");
+    if (numUser == numParent) {
+                    div1.innerHTML = "The numbers are equals";
+                    div2.innerHTML = "The numbers are equals";
+                } else {
+                    div1.innerHTML = "The numbers doesn't match";
+                    div2.innerHTML = "The numbers doesn't match";
+                }
+}
+
+// document.getElementById("compare").onclick = function () {
+//     window.displayRandomNumber = (number) => {
+//         let numUser = parseInt(document.getElementById("num").value);
+//         div2 = document.getElementById("resolve");
+//         if (numUser == number) {
+//             div1.innerHTML = "The numbers are equals";
+//             div2.innerHTML = "The numbers are equals";
+//         } else {
+//             div1.innerHTML = "The numbers doesn't match";
+//             div2.innerHTML = "The numbers doesn't match";
+//         }
+//     }
+// };
+
 
